c5040362a59a609578f7efc36ee27af6a01b5a36
Virginia Silva <virginia_hol@hotmail.com>
2022-02-07 16:04:54 -0300

    first commit

---
+++ src/TestaConexao.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestaConexao {
+
+	public static void main(String[] args) throws SQLException {
+
+		Connection con = DriverManager.
+				getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "1234");
+		
+		System.out.println("teste");
+
+		con.close();
+	}
+
+}
